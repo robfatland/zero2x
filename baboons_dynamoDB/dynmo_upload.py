995983feb4317672470693cc75e9f4f151bd8652
robfatland <rob5@uw.edu>
2019-04-08 12:29:10 -0700

    added data

--- baboons_dynamoDB/dynmo_upload.py
+++ baboons_dynamoDB/dynmo_upload.py
@@ -1,3 +1,5 @@
+
+
 """
     Utility script to load data into dynamodb.
 """
@@ -35,7 +37,7 @@ def dynamodb_upload(data_frame):
     mydata=data_frame.T.to_dict().values() # converting dataframe to a dict for DynamoDB to consume
     resource = boto3.resource('dynamodb', aws_access_key_id=creds_data['key_id'],
          aws_secret_access_key=creds_data['key_access'], region_name='us-west-2')
-    table = resource.Table('baboons') # table name to push the data
+    table = resource.Table('amboseli') # table name to push the data
     with table.batch_writer() as batch: # batch writer
         for row in tqdm(mydata):    #progress bar                                                                                                               
             batch.put_item(Item=row)
@@ -43,7 +45,7 @@ def dynamodb_upload(data_frame):
 
 if __name__=='__main__':
     num_process = 10
-    data = pd.read_csv('baboons.csv') #S3 location of baboons dataset.
+    data = pd.read_csv('./baboons.csv') #S3 location of baboons dataset.
     split_df = np.array_split(data, num_process) # splitting data into  smaller data frames for each process.
     del data 
     gc.collect() # garbage collection to keep memeory usage in check.
