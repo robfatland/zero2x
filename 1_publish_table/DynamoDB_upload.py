12564929c522cc0d4957b5341007dd8394501d28
robfatland <rob5@uw.edu>
2019-04-09 13:24:51 -0700

    fixed up upload.py

--- 1_publish_table/DynamoDB_upload.py
+++ 1_publish_table/DynamoDB_upload.py
@@ -31,23 +31,25 @@ def dynamodb_upload(data_frame):
         Utility function to upload data to dynamodb
     """
     creds_data = load_creds()
-    col_to_string = ['x', 'y', 'indiv'] #converting columns to string for DynamoDB
-    for i in col_to_string:                                                                                                                        
+    col_to_string = ['x', 'y', 'indiv']            # converting columns to string for DynamoDB
+    for i in col_to_string: 
         data_frame[i] = data_frame[i].astype(str)
-    mydata=data_frame.T.to_dict().values() # converting dataframe to a dict for DynamoDB to consume
-    resource = boto3.resource('dynamodb', aws_access_key_id=creds_data['key_id'],
-         aws_secret_access_key=creds_data['key_access'], region_name='us-west-2')
-    table = resource.Table('amboseli') # table name to push the data
-    with table.batch_writer() as batch: # batch writer
-        for row in tqdm(mydata):    #progress bar                                                                                                               
+    mydata=data_frame.T.to_dict().values()         # dataframe --> dict for DynamoDB to consume
+    resource = \
+        boto3.resource('dynamodb', 
+                       aws_access_key_id=creds_data['key_id'],
+                       aws_secret_access_key=creds_data['key_access'], 
+                       region_name='us-west-2')
+    table = resource.Table('baboons1')             # table name to push the data
+    with table.batch_writer() as batch:            # batch writer
+        for row in tqdm(mydata):                   # progress bar 
             batch.put_item(Item=row)
-
 
 if __name__=='__main__':
     num_process = 10
-    data = pd.read_csv('./baboons.csv') #S3 location of baboons dataset.
-    split_df = np.array_split(data, num_process) # splitting data into  smaller data frames for each process.
+    data = pd.read_csv('./baboons.csv')            # location of CSV file (local, S3, etc).
+    split_df = np.array_split(data, num_process)   # splits data into data frames, one for each process
     del data 
-    gc.collect() # garbage collection to keep memeory usage in check.
+    gc.collect()                                   # garbage collection: reduce memory consumption 
     pool = Pool(processes = num_process)
     pool.map(dynamodb_upload, split_df)
